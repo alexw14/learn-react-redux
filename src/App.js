e3ec3528c5afdda31b1d9468ae29f0c185bf2a87
Alex Wong <alexw14@gmail.com>
2018-02-27 20:49:56 -0800

    Create Person component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Alex', age: "28" },
+      { name: 'Kobe', age: "8" },
+      { name: 'Bryant', age: "24" },
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
 
