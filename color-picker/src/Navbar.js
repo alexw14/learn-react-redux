b9379dba853196ece208c1fa47b247e407652064
Alex Wong <alexw14@gmail.com>
2019-09-24 23:20:54 -0700

    Add snackbar component to Navbar

--- color-picker/src/Navbar.js
+++ color-picker/src/Navbar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
@@ -10,13 +13,21 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      format: 'hex'
+      format: 'hex',
+      open: false
     };
   }
 
-  handleChange = (evt) => {
-    this.setState({ format: evt.target.value });
+  handleFormatChange = (evt) => {
+    this.setState({
+      format: evt.target.value,
+      open: true
+    });
     this.props.handleChange(evt.target.value);
+  }
+
+  closeSnackbar = () => {
+    this.setState({ open: false });
   }
 
   render() {
@@ -40,12 +51,27 @@ class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-          <Select value={format} onChange={this.handleChange}>
+          <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX</MenuItem>
             <MenuItem value="rgb">RGB</MenuItem>
             <MenuItem value="rgba">RGBA</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={<span id="message-id">Format changed to {format.toUpperCase()}!</span>}
+          ContentProps={{
+            "aria-describedby": "message-id"
+          }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton onClick={this.closeSnackbar} color='inherit' key="close" aria-label="close">
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </div>
     );
   }
