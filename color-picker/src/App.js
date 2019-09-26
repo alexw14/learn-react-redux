8b24381de5e2d7e8d15a340ad9197de4a280ce40
Alex Wong <alexw14@gmail.com>
2019-09-25 23:30:31 -0700

    Add react-router-dom and add route to App

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 function App() {
+
+  const findPalette = (id) => {
+    return seedColors.find((palette) => {
+      return palette.id === id;
+    });
+  }
+
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route
+        exact
+        path='/'
+      />
+      <Route
+        exact
+        path='/palette/:id'
+        render={(routeProps) => (
+          <Palette
+            palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        )}
+      />
+    </Switch>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
